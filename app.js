25d1248e5b61a787752386882e56cb0875c29b3f
b.palowski <brianpalowski00@gmail.com>
2019-02-12 20:07:51 -0800

    Updated pt 2 of find git

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const http = new Git;
-// Search Input
+// Init Github
+const github = new Github;
+
+// Search input
 const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-	// get input text
+	// Get input text
 	const userText = e.target.value;
+
 	if (userText !== '') {
-		// HTTP call
-		http.getUser(userText)
+		// Make http call
+		github.getUser(userText)
 			.then(data => {
-				console.log(data);
+				if (data.profile.message === 'Not Found') {
+					// Show alert
+
+				} else {
+					// Show profile
+				}
 			})
+	} else {
+		// Clear profile
+
 	}
-})
+}); 
