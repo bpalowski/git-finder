6b5091d88241a1ef045f106cf9e61c823abb59d4
b.palowski <brianpalowski00@gmail.com>
2019-02-13 14:42:22 -0800

    Updated error

--- ui.js
+++ ui.js
@@ -2,7 +2,7 @@ class UI {
 	constructor() {
 		this.profile = document.getElementById('profile');
 	}
-
+	// display Profile UI
 	showProfile(user) {
 		this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -29,5 +29,42 @@ class UI {
       <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
     `;
+	}
+	// show Alert message
+
+	showAlert(message, className) {
+		/// Clear any remaining alert
+		this.clearAlert();
+		/// Div 
+		const div = document.createElement('div');
+		div.className = `${className}`;
+		// text 
+		div.appendChild(document.createTextNode(message));
+		// get parent
+		const container = document.querySelector('.searchContainer');
+
+		// get search box
+		const search = document.querySelector('.search');
+		// insert alert
+		container.insertBefore(div, search);
+
+		// timeout
+		setTimeout(() => {
+			this.clearAlert();
+		}, 3000);
+	}
+
+	// Clear Alert Message
+	clearAlert() {
+		const currentAlert = document.querySelector('.alert');
+
+		if (currentAlert) {
+			currentAlert.remove();
+		}
+	}
+
+	/// clear profile
+	clearProfile() {
+		this.profile.innerHTML = '';
 	}
 }
